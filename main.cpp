309493a1a604d94a0a88ea04c2b04cd1d9460c30
Krishna Patil <krishnapatil6446@gmail.com>
2024-06-17 22:40:08 +0530

    first commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  C++First
+//
+//  Created by Krishna Patil on 14/04/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
