9f861891f5bf43524bc1e23cbe06aa17d33e1ba2
Krishna Patil <krishnapatil6446@gmail.com>
2024-06-19 11:43:17 +0530

    Program added

---
+++ else-if.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+int a;
+cin>>a;
+if(a>0){
+    cout<<a<<" is a positive number";
+}
+else if (a<0)
+{
+    cout<<a<<" is a negative number";
+}
+else{
+    cout<<a<<" is zero";
+}
+
+
+}
