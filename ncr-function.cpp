1ad8658f415ed3ad9f50da11598a7dee8db2939a
Krishna Patil <krishnapatil6446@gmail.com>
2024-07-31 10:53:48 +0530

    functions added

---
+++ ncr-function.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int factorial(int n){
+    int fact = 1;
+    for (int i = 1; i<=n;i++){
+        fact = fact * i;
+    }
+    return fact;
+}
+
+int nCr(int n, int r){
+    int numerator = factorial(n);
+    int denominator = factorial(r) * factorial(n-r);
+    return numerator/denominator;
+}
+
+int main (){
+int n,r;
+cin >> n >> r;
+cout << "Answer is : " << nCr(n,r) << endl;
+}
